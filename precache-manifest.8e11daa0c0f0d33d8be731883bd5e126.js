2a367ce67b35384e6b1a37b813da1317ec5ddd55
Evgeny Berezhnov <jackberezhnov@gmail.com>
2020-04-19 23:33:20 -0700

    Updates

---
+++ precache-manifest.8e11daa0c0f0d33d8be731883bd5e126.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4dc23af0a610211c1fa4067fc3e24af",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ab8475a9f58d79128caf",
+    "url": "/monsters-rolodex/static/css/main.c206ad49.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab8475a9f58d79128caf",
+    "url": "/monsters-rolodex/static/js/main.c1517221.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
